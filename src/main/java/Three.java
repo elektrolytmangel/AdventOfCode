360d34cc9c74a7d0603d0002f9d790c4e8039fac
marc <marc.muehlheim@hftm.ch>
2019-12-03 22:44:22 +0100

    work in progress

--- src/main/java/Three.java
+++ src/main/java/Three.java
@@ -2,8 +2,28 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Three implements AdventOfCode {
+
+    public static void main(String[] args){
+        Three three = new Three("src/main/resources/three.txt");
+        ArrayList<ArrayList<String>> wiresWithPositions = new ArrayList<>();
+        for (String wire : three.inputValues){
+            wiresWithPositions.add(three.calculatePositions(Three.convertCsvToArrayList(wire)));
+        }
+        System.out.println("find equal positions");
+        ArrayList<String> equalPositions = three.findEqualPositions(wiresWithPositions.get(0), wiresWithPositions.get(1));
+        System.out.println(equalPositions);
+        System.out.println("calculate distances");
+        ArrayList<Integer> distances = new ArrayList<>();
+        for (String pos : equalPositions){
+            distances.add(three.calculateDistanceToZero(pos));
+        }
+        System.out.println("sort distances");
+        Collections.sort(distances);
+        System.out.println(distances);
+    }
 
     ArrayList<String> inputValues;
 
@@ -43,34 +63,58 @@ public class Three implements AdventOfCode {
         for (String posArg : wire){
             String instr = posArg.substring(0,1);
             int distance = Integer.parseInt(posArg.substring(1));
+            System.out.println("Current pos: " + x + ", " + y);
+            System.out.println("Args: orig=" + posArg + " case=" + instr + " dist=" + distance );
             switch (instr){
                 case "U":
-                    y += distance;
+                    for (int i = 0; i < distance; i++){
+                        y++;
+                        wirePoints.add(x + "," + y);
+                    }
                     break;
                 case "D":
-                    y -= distance;
+                    for (int i = 0; i < distance; i++){
+                        y--;
+                        wirePoints.add(x + "," + y);
+                    }
                     break;
                 case "R":
-                    x += distance;
+                    for (int i = 0; i < distance; i++){
+                        x++;
+                        wirePoints.add(x + "," + y);
+                    }
                     break;
                 case "L":
-                    x -= distance;
+                    for (int i = 0; i < distance; i++){
+                        x--;
+                        wirePoints.add(x + "," + y);
+                    }
                     break;
                 default:
                     throw new IllegalArgumentException("not a valid instruction");
             }
-            wirePoints.add(x + "," + y);
         }
         return wirePoints;
     }
 
+//    public boolean isVectorCrossing(Vector vector1, Vector vector2){
+//        boolean xCross = false;
+//        boolean yCross = true;
+//        if (vector1.x1 < vector2.x1 && )
+//    }
+
     public ArrayList<String> findEqualPositions(ArrayList<String> positionsOne, ArrayList<String> positionsTwo){
         ArrayList<String> crossPos = new ArrayList<>();
-        for (String pos : positionsOne){
-            for (String pos2 : positionsTwo){
-                if (pos2.equals(pos)){
-                    crossPos.add(pos);
-                }
+        System.out.println(positionsOne.size());
+        System.out.println(positionsTwo.size());
+        String posBefore;
+        String posAfter;
+        for (int i = 0; i < positionsOne.size(); i++) {
+            String pos = positionsOne.get(i);
+            if (positionsTwo.contains(pos)) {
+                int pos2Idx = positionsTwo.indexOf(pos);
+
+                crossPos.add(pos);
             }
         }
         return crossPos;
@@ -81,5 +125,19 @@ public class Three implements AdventOfCode {
         int x = Integer.parseInt(xys[0]);
         int y = Integer.parseInt(xys[1]);
         return x + y;
+    }
+
+    public class Vector{
+        int x1;
+        int x2;
+        int y1;
+        int y2;
+
+        public Vector(int x1, int x2, int y1, int y2) {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
     }
 }
--- src/test/java/ThreeTests.java
+++ src/test/java/ThreeTests.java
@@ -1,27 +1,62 @@
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class ThreeTests {
 
     @Test
+    public void firstStarGivenExample1(){
+        Three three = new Three("src/main/resources/three.txt");
+        ArrayList<String> first = Three.convertCsvToArrayList("R75,D30,R83,U83,L12,D49,R71,U7,L72");
+        ArrayList<String> second = Three.convertCsvToArrayList("U62,R66,U55,R34,D71,R55,D58,R83");
+
+        ArrayList<String> firstPos = three.calculatePositions(first);
+        ArrayList<String> secondPos = three.calculatePositions(second);
+
+        ArrayList<String> equals = three.findEqualPositions(firstPos, secondPos);
+        System.out.println(equals);
+        ArrayList<Integer> distances = new ArrayList<>();
+        for (String e : equals){
+            distances.add(three.calculateDistanceToZero(e));
+        }
+        System.out.println(distances);
+    }
+
+    @Test
+    public void firstStarGivenExample2(){
+        Three three = new Three("src/main/resources/three.txt");
+        ArrayList<String> first = Three.convertCsvToArrayList("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
+        ArrayList<String> second = Three.convertCsvToArrayList("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
+
+        ArrayList<String> firstPos = three.calculatePositions(first);
+        ArrayList<String> secondPos = three.calculatePositions(second);
+
+        ArrayList<String> equals = three.findEqualPositions(firstPos, secondPos);
+        System.out.println(equals);
+        ArrayList<Integer> distances = new ArrayList<>();
+        for (String e : equals){
+            distances.add(three.calculateDistanceToZero(e));
+        }
+        System.out.println(distances);
+    }
+
+    @Test
     public void getFirstStarResult(){
         Three three = new Three("src/main/resources/three.txt");
         ArrayList<ArrayList<String>> wiresWithPositions = new ArrayList<>();
         for (String wire : three.inputValues){
-            System.out.println(wire);
             wiresWithPositions.add(three.calculatePositions(Three.convertCsvToArrayList(wire)));
         }
-        System.out.println(wiresWithPositions.get(0));
-        System.out.println(wiresWithPositions.get(1));
+        System.out.println("find equal positions");
         ArrayList<String> equalPositions = three.findEqualPositions(wiresWithPositions.get(0), wiresWithPositions.get(1));
-        System.out.println(equalPositions);
+        System.out.println("calculate distances");
         ArrayList<Integer> distances = new ArrayList<>();
         for (String pos : equalPositions){
             distances.add(three.calculateDistanceToZero(pos));
         }
-
+        System.out.println("sort distances");
         Collections.sort(distances);
         System.out.println(distances);
     }
