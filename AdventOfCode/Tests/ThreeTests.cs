05c8612290bf52a4b0ad3ddba85412f21ff1453e
MUEHLHEIM Marc <marc.muehlheim@hexagonsi.com>
2020-12-03 08:06:30 +0100

    add day3

---
+++ AdventOfCode/Tests/ThreeTests.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Puzzels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class ThreeTests
+    {
+        [Test]
+        public void GetResultOfFirstPuzzle()
+        {
+            var three = new Three("./Input/Three.txt");
+            var res = three.Part1();
+            Assert.AreEqual(252, res);
+            System.Console.WriteLine($"The result of day three / first puzzel is: {res}");
+        }
+
+        [Test]
+        public void Part1ControlResult()
+        {
+            var three = new Three("./Input/ThreeTest.txt");
+            var res = three.Part1();
+
+            Assert.AreEqual(7, res);
+        }
+
+        [Test]
+        public void GetResultOfSecondPuzzle()
+        {
+            var three = new Three("./Input/Three.txt");
+            var res = three.Part2();
+            Assert.AreEqual(2608962048, res);
+            System.Console.WriteLine($"The result of day three / second puzzel is: {res}");
+        }
+    }
+}
