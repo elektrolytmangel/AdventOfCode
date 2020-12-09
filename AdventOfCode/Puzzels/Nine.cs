8cb4c3be001239dfa604751504deff17b1e1766e
MUEHLHEIM Marc <marc.muehlheim@hexagonsi.com>
2020-12-09 09:33:20 +0100

    add solution from day9

---
+++ AdventOfCode/Puzzels/Nine.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Puzzels
+{
+    public class Nine : AdventOfCodeBase
+    {
+        private List<long> numbers = new List<long>();
+
+        public Nine(string path)
+        {
+            this.ReadFile(path);
+            foreach (var numStr in this.InputValues)
+            {
+                numbers.Add(long.Parse(numStr));
+            }
+        }
+
+        public long Part1(int preamble = 25)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                var numberIdxToCheck = i + preamble;
+                // check if idx to check exists
+                if (numbers.Count > numberIdxToCheck)
+                {
+                    var numberToCheck = numbers[numberIdxToCheck];
+                    var isSum = IsSumOfPreviouse(numberToCheck, i, preamble);
+                    if (!isSum)
+                    {
+                        return numberToCheck;
+                    }
+                }
+            }
+
+            return -1L;
+        }
+
+        public long Part2(int preamble = 25)
+        {
+            var invalidNumber = this.Part1(preamble);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                var summands = new List<long>();
+                for (int j = i; j < numbers.Count; j++)
+                {
+                    summands.Add(numbers[j]);
+
+                    long sum = 0;
+                    summands.ForEach(x => sum += x);
+                    //Console.WriteLine($"invalidNumber: {invalidNumber}, sum:{sum}, summands: {string.Join(", ", summands)}");
+                    if (sum == invalidNumber)
+                    {
+                        Console.WriteLine($"invalidNumber: {invalidNumber}, sum:{sum}, summands: {string.Join(", ", summands)}");
+
+                        return summands.Min() + summands.Max();
+                    }
+                }
+            }
+
+            return -1L;
+        }
+
+        private bool IsSumOfPreviouse(long testSum, int startIndex, int preamble)
+        {
+            var estEndIdx = startIndex + preamble;
+            var endIdx = numbers.Count <= estEndIdx ? numbers.Count - 1 : estEndIdx;
+            for (int i = startIndex; i < endIdx; i++)
+            {
+                var firstNum = numbers[i];
+                var otherNum = testSum - firstNum;
+                var currentNumberArr = numbers.GetRange(startIndex, preamble);
+                if (currentNumberArr.IndexOf(otherNum) != -1 && otherNum != firstNum)
+                {
+                    return true;
+                }
+            }          
+
+            return false;
+        }
+    }
+}
---
+++ AdventOfCode/Tests/NineTests.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode.Puzzels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class NineTests
+    {
+        [Test]
+        public void GetResultOfFirstPuzzle()
+        {
+            var nine = new Nine("./Input/Nine.txt");
+            var res = nine.Part1();
+            //Assert.AreEqual(252, res);
+            System.Console.WriteLine($"The result of day Nine / first puzzel is: {res}");
+        }
+
+        [Test]
+        public void Part1ControlResult()
+        {
+            var nine = new Nine("./Input/NineTest.txt");
+            var res = nine.Part1(5);
+            System.Console.WriteLine($"The result of day Nine / first puzzel is: {res}");
+
+            Assert.AreEqual(127, res);
+        }
+
+        [Test]
+        public void Part2ControlResult()
+        {
+            var nine = new Nine("./Input/NineTest.txt");
+            var res = nine.Part2(5);
+            System.Console.WriteLine($"The result of day Nine / first puzzel is: {res}");
+
+            Assert.AreEqual(62, res);
+        }
+
+        [Test]
+        public void GetResultOfSecondPuzzle()
+        {
+            var nine = new Nine("./Input/Nine.txt");
+            var res = nine.Part2();
+            //Assert.AreEqual(252, res);
+            System.Console.WriteLine($"The result of day Nine / second puzzel is: {res}");
+        }
+    }
+}
