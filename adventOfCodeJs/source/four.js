2da52a21df1dcb9c0361726849e17f126a9c6718
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2021-12-08 16:18:52 +0100

    try to add day04

--- adventOfCodeJs/source/four.js
+++ adventOfCodeJs/source/four.js
@@ -1,11 +1,8 @@
 import { getLines } from "./modules/input.js";
 
-let inputValues = getLines("four");
+let inputValues = getLines("fourTest");
 
-const part1 = () => {
-    let bingoNumbers = inputValues[0].split(',').map(x => parseInt(x));
-    let height = 5;
-    let width = 5;
+const getBingoFields = () => {
     let bingoFields = [];
     let currentField = []
     for (let i = 1; i < inputValues.length; i++) {
@@ -20,39 +17,108 @@ const part1 = () => {
         }
     }
 
-    bingoFields = bingoFields.filter(a => a.length != 0);
+    return bingoFields.filter(a => a.length != 0);
+};
+
+const getBingoNumbers = () => {
+    return inputValues[0].split(',').map(x => parseInt(x));
+}
+
+const part1 = () => {
+    let bingoNumbers = getBingoNumbers();
+    let bingoFields = getBingoFields();
 
     // works only for horizontal rows
     for (let i = 0; i < bingoNumbers.length; i++) {
         let currentNumber = bingoNumbers[i];
         for (let field of bingoFields) {
             for (let row of field) {
-                let rowCompletionRate = 0;
-                for (let number of row) {
-                    if (number.n == currentNumber) {
-                        number.c += 1;
+                let horizontalRowCompletionRate = 0;
+                let verticalRowCompletionRate = [0, 0, 0, 0, 0];
+                let verticalRowCompleted = false;
+                for (let number in row) {
+                    if (row[number].n == currentNumber) {
+                        row[number].c += 1;
                     }
 
-                    if (number.c > 0) {
-                        rowCompletionRate++;
+                    if (row[number].c > 0) {
+                        horizontalRowCompletionRate++;
+                        verticalRowCompletionRate[number]++
+                    }
+
+                    verticalRowCompleted = verticalRowCompletionRate.find(x => x == 5) != null;
+
+                    if (verticalRowCompleted) {
+                        break;
                     }
                 }
 
-                if (rowCompletionRate == 5) {
+                if (horizontalRowCompletionRate == 5 || verticalRowCompleted) {
                     let notHitNumbers = [];
                     field.forEach(n => n.forEach(e => {
                         if (e.c < 1) {
                             notHitNumbers.push(e.n);
                         }
                     }));
+
                     return currentNumber * notHitNumbers.reduce((a, b) => a + b);
                 }
             }
         }
     }
+}
 
-    console.log(0)
+
+const part2 = () => {
+    let bingoNumbers = getBingoNumbers();
+    let bingoFields = getBingoFields();
+
+    // works only for horizontal rows
+    for (let i = 0; i < bingoNumbers.length; i++) {
+        let currentNumber = bingoNumbers[i];
+        for (let field in bingoFields) {
+            for (let row of bingoFields[field]) {
+                let horizontalRowCompletionRate = 0;
+                let verticalRowCompletionRate = [0, 0, 0, 0, 0];
+                let verticalRowCompleted = false;
+                for (let number in row) {
+                    if (row[number].n == currentNumber) {
+                        row[number].c += 1;
+                    }
+
+                    if (row[number].c > 0) {
+                        horizontalRowCompletionRate++;
+                        verticalRowCompletionRate[number]++
+                    }
+
+                    verticalRowCompleted = verticalRowCompletionRate.find(x => x == 5) != null;
+
+                    if (verticalRowCompleted) {
+                        break;
+                    }
+                }
+
+                if (horizontalRowCompletionRate == 5 || verticalRowCompleted) {
+                    if (bingoFields.length > 1) {
+                        bingoFields.splice(field, 1)
+                    }
+                    else {
+                        let notHitNumbers = [];
+                        bingoFields[field].forEach(n => n.forEach(e => {
+                            if (e.c < 1) {
+                                notHitNumbers.push(e.n);
+                            }
+                        }));
+
+                        return currentNumber * notHitNumbers.reduce((a, b) => a + b);
+                    }
+                }
+            }
+        }
+    }
 }
 
 let result = part1();
 console.log(result)
+let result2 = part2();
+console.log(result2)
